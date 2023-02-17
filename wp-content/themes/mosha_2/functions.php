b14df2314b730e9a6cbaf0b1cfd33be6e991aa39
Nora <nsorym04@gmail.com>
2023-02-17 16:09:44 +0900

    header完成

--- wp-content/themes/mosha_2/functions.php
+++ wp-content/themes/mosha_2/functions.php
@@ -1,7 +1,7 @@
 <?php
-function styles()
+function load_my_styles()
 {
-    wp_enqueue_style('style', get_template_directory_uri('/css/style.css'));
-    wp_enqueue_style('normalize', get_template_directory_uri('/css/normalize.css'));
+    wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array('style'));
 }
-add_action('wp_enqueue_scripts', 'styles');
+add_action('wp_enqueue_scripts', 'load_my_styles');
---
+++ wp-content/themes/mosha_2/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body class="main-body">
+    <header class="wrapper">
+        <div class="logo"><a href="<?php echo esc_url(home_url()) ?>"><img src="<?php echo esc_url(get_theme_file_uri('img/logo.svg')) ?>" alt=""></a></div>
+        <ul class="category">
+            <li><a href="<?php echo esc_url(home_url('/category/news')) ?>">NEWS</a></li>
+            <li><a href="<?php echo esc_url(home_url()) ?>#about">ABOUT</a></li>
+            <li><a href="<?php echo esc_url(home_url()) ?>#business">BUSINESS</a></li>
+            <li><a href="<?php echo esc_url(home_url()) ?>#company">COMPANY</a></li>
+        </ul>
+        <div class="contact-link"><a href="<?php esc_url(home_url('/contact')) ?>">お問い合わせ</a></div>
+    </header>
