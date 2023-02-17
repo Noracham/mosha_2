1cf62d8bc8fe9a6112bf3d00b72ee1d5fc4ab91a
Nora <nsorym04@gmail.com>
2023-02-18 02:16:31 +0900

    コラム表示ページの切り替えリンクリストの作成

---
+++ wp-content/themes/mosha_2/category.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<main class="wrapper2">
+    <h1 class="c-sub">News & Press</h1>
+    <div class="n-p">
+        <ul class="n-p-change">
+            <li><a href="<?php echo esc_url(home_url('/category/news')) ?>">ニュース</a></li>
+            <li><a href="<?php echo esc_url(home_url('/category/press')) ?>">プレスリリース</a></li>
+        </ul>
+    </div>
+</main>
+<?php get_footer(); ?>
