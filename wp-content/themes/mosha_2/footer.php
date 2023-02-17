2a6097ec8b08c95b87fbff85c117fd213a1801b2
Nora <nsorym04@gmail.com>
2023-02-18 00:42:14 +0900

    footer完成

--- wp-content/themes/mosha_2/footer.php
+++ wp-content/themes/mosha_2/footer.php
@@ -1,3 +1,19 @@
+<footer>
+    <div class="wrapper2">
+        <div class="footer-info">
+            <div class="footer-logo">
+                <img src="<?php echo esc_url(get_theme_file_uri('img/logo.svg')) ?>" alt="">
+            </div>
+            <p>Web Entertainment Design Inc.<br>
+                West Building 3F<br>
+                9-99 Sakuragaokacho Shibuya-ku<br>
+                Tokyo, Japan 150-0031<br>
+                T/99-9999-9999</p>
+        </div>
+        <p>&copy;Web Entertainment Design Inc.</p>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 
